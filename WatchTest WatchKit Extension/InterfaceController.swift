239c6aba9d58f912b46431704071faa7b6e5e93b
Nicholas Smith <npsmith1990@gmail.com>
2016-01-06 18:05:21 -0600

    Add table row and associated class

--- WatchTest WatchKit Extension/InterfaceController.swift
+++ WatchTest WatchKit Extension/InterfaceController.swift
@@ -12,20 +12,11 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var table: WKInterfaceTable!
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
+        self.table.setNumberOfRows(3, withRowType: "type")
     }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
 }
---
+++ WatchTest WatchKit Extension/TableRow.swift
@@ -0,0 +1,17 @@
+//
+//  TableRow.swift
+//  WatchTest
+//
+//  Created by Nick on 1/6/16.
+//  Copyright © 2016 Nick. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class TableRow: NSObject {
+
+    @IBOutlet var rowLbl: WKInterfaceLabel!
+
+    
+}
